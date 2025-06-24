c2d56cc7d54a03ac9d7c08e59d6066dfb9892431
Dr. Desmond <adesmondtoye@gmail.com>
2025-06-24 16:15:00 -0700

    add env pkg

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { MemoryStore } from './storage/memory.store';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, MemoryStore],
 })
